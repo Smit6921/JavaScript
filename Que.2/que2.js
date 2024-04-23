e4884b9a43a757d858f7a81cd35b61d0cef33f55
Smit <6921smitupadhyay@gmail.com>
2024-04-23 18:24:26 +0530

    Project 1

--- Que.2/que2.js
+++ Que.2/que2.js
@@ -1,7 +1,7 @@
 function variableSwap() 
 {
-    let a = prompt("Enter the value of variable a:");
-    let b = prompt("Enter the value of variable b:");
+    let a = prompt("Enter the value of a:");
+    let b = prompt("Enter the value of b:");
     
     console.log("Before swapping:");
     console.log("a =", a);
