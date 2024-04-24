fec67e413164fb8713978040d3fa97bece3807c0
Smit <6921smitupadhyay@gmail.com>
2024-04-24 18:00:18 +0530

    a

---
+++ Project 1/Que.1/Que1.js
@@ -0,0 +1,22 @@
+// 1st pattern Start
+document.write("<pre>           *           *</pre>");
+document.write("<pre>     *           *</pre>");
+document.write("<pre> *         *</pre>");
+document.write("<pre>     *           *</pre>");
+document.write("<pre>           *           *</pre><br> <br>");
+// 1st Pattern End
+
+
+// 2nd Pattern Start
+document.write("<pre>      *     *     *     *     *     *     *</pre>");
+
+        document.write("<pre>*     *     *     *     *     *     *     *</pre>");
+
+        document.write("<pre>*           *                             *</pre>"); 
+
+        document.write("<pre>*           *           *     *     *     *</pre>");
+
+        document.write("<pre>*           *           *           *     *</pre>");
+
+        document.write("<pre>*     *     *     *     *     *     *     *</pre>");
+// 2nd Pattern End
