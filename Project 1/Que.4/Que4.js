e72911e066434e9def61efeb71a8eaa19ff78279
Smit <6921smitupadhyay@gmail.com>
2024-04-24 18:01:21 +0530

    Project 1

--- Project 1/Que.4/Que4.js
+++ Project 1/Que.4/Que4.js
@@ -1,16 +1,19 @@
-function calculateGrossSalary(basicSalary, allowances, deductions) {
+function calculateGrossSalary(basicSalary, allowances, deductions)
+ {
     let grossSalary = basicSalary + allowances - deductions;
     return grossSalary;
 }
 
-function getUserInput() {
+function getUserInput()
+ {
     let basicSalary = parseFloat(prompt("Enter basic salary:"));
     let allowances = parseFloat(prompt("Enter allowances:"));
     let deductions = parseFloat(prompt("Enter deductions:"));
     
     return { basicSalary, allowances, deductions };
 }
-function main() {
+function main() 
+{
     const { basicSalary, allowances, deductions } = getUserInput();
     if (isNaN(basicSalary) || isNaN(allowances) || isNaN(deductions)) {
         console.log("Invalid input. Please enter numeric values.");
