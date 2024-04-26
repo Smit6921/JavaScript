26db5dee7c151e94797e83a7b9ed2561b95de37e
Smit6921 <6921smitupadhyay@gmail.com>
2024-04-26 16:14:07 +0530

    Project 1

--- Project 1/Que.1/Que1.js
+++ Project 1/Que.1/Que1.js
@@ -1,9 +1,23 @@
 // 1st pattern Start
-document.write("<pre>           *           *</pre>");
-document.write("<pre>     *           *</pre>");
-document.write("<pre> *         *</pre>");
-document.write("<pre>     *           *</pre>");
-document.write("<pre>           *           *</pre><br> <br>");
+const size = 5;
+
+console.log("Pattern 1");
+for (let i = 0; i < size; i++) {
+    let row = '';
+
+    for (let j = 0; j <= size; j++) {
+        if ((i === 0 && (j === 3 || j === 5)) ||
+            (i === 1 && (j === 2 || j === 4)) ||
+            (i === 2 && (j === 1 || j === 3)) ||
+            (i === 3 && (j === 2 || j === 4)) ||
+            (i === 4 && (j === 3 || j === 5))) {
+            row += '*   ';
+        } else {
+            row += '    ';
+        }
+    }
+    console.log(row);
+}
 // 1st Pattern End
 
 
