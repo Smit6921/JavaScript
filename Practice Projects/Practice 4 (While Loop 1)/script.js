ec7203f045ddaa313fc7e1354cd3d191c028bd6a
Smit <6921smitupadhyay@gmail.com>
2024-04-30 18:11:52 +0530

    Practice (While Loop)

---
+++ Practice Projects/Practice 4 (While Loop 1)/script.js
@@ -0,0 +1,9 @@
+let i = 2;
+let count = 0;
+
+while(i <= 9) {
+    console.log(i)
+    i++
+    count++
+}
+console.log(count)
---
+++ Practice Projects/Practice 4 (While Loop 2)/script.js
@@ -0,0 +1,12 @@
+myVar = "smit";
+let count = 0;
+
+while(true) {
+    count ++
+    if(myVar == "") {
+        break
+    }
+    myVar = ""
+}
+
+console.log(`count:`,count)
