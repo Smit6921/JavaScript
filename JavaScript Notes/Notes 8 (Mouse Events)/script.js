20cc82e4463448ce863f6238f7cd5f3f34c9dd59
Smit6921 <6921smitupadhyay@gmail.com>
2024-06-11 15:30:06 +0530

    Notes

---
+++ JavaScript Notes/Notes 8 (Mouse Events)/script.js
@@ -0,0 +1,41 @@
+// Mouse Events (onclick, ondblclick, onContextMenu, onMouseOver, onMouseLeave, onSubmit)
+
+
+// OnClick Event()
+
+const test = () => {
+    p1.innerHTML = "Hi"
+}
+
+// OnDouble Click Event ()
+
+const test1 = () => {
+    p2.innerHTML = "Hello"
+}
+
+
+// OnContextMenu Event ()
+
+const test2 = () => {
+    p3.innerHTML = "Hello World"
+}
+
+
+// OnMouseOver Event ()
+
+const test3 = () => {
+    p4.innerHTML = "Hello It's Me"
+}
+
+
+// OnMouseLeave Event ()
+
+const test4 = () => {
+    p5.innerHTML = "Welcome"
+}
+
+// OnSubmit Event ()
+
+const test5 = () => {
+   console.log("Your Message")
+}
