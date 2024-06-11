28ce82387accadb458a973dd2368d0c77882dbc1
Smit6921 <6921smitupadhyay@gmail.com>
2024-06-11 12:43:06 +0530

    Notes

---
+++ JavaScript Notes/Notes 6 (Query Selector & Query Selector All)/script.js
@@ -0,0 +1,11 @@
+// Query Selector
+
+let para1 = document.querySelector("#para1")
+para1.innerHTML = "Hello"
+
+
+// Query Selector All
+
+let para2 = document.querySelectorAll("p")
+para2.innerHTML = "Hii"
+console.log(para2)
